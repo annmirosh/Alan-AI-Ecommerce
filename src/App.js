3a62a6a90df54d3dee77918d77ce412c1cfe2102
Web Dev Simplified <webdevsimplified@gmail.com>
2020-11-23 10:37:23 -0600

    WIP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import storeItems from "./items.json"
+import Store from "./components/Store"
+import Cart from "./components/Cart"
+import useAlan from "./hooks/useAlan"
 
 function App() {
+  useAlan()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Store items={storeItems} />
+      <Cart />
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/hooks/useAlan.js
@@ -0,0 +1,205 @@
+// import { useRef } from "react"
+// import alanBtn from "@alan-ai/alan-sdk-web"
+// import { useCart } from "../context/CartContext"
+// import storeItems from "../items.json"
+
+// const COMMANDS = {
+//   OPEN_CART: "open-cart",
+//   CLOSE_CART: "close-cart",
+//   ADD_ITEM: "add-item",
+//   REMOVE_ITEM: "remove-item",
+//   PURCHASE_ITEMS: "purchase-items"
+// }
+
+// export default function useAlan() {
+//   const {
+//     cart,
+//     isCartEmpty,
+//     setShowCartItems,
+//     addToCart,
+//     removeFromCart,
+//     checkout
+//   } = useCart()
+
+//   const alan = useRef()
+//   if (alan.current == null) {
+//     alan.current = alanBtn({
+//       key: process.env.REACT_APP_ALAN_KEY,
+//       onCommand: ({ command, payload }) => {
+//         switch (command) {
+//           case COMMANDS.OPEN_CART:
+//             openCart(alan)
+//             break
+//           case COMMANDS.CLOSE_CART:
+//             closeCart(alan)
+//             break
+//           case COMMANDS.ADD_ITEM:
+//             addItem(alan, payload)
+//             break
+//           case COMMANDS.REMOVE_ITEM:
+//             removeItem(alan, payload)
+//             break
+//           case COMMANDS.PURCHASE_ITEMS:
+//             purchaseItems(alan)
+//             break
+//         }
+//       }
+//     })
+//   }
+
+//   function openCart(alan) {
+//     if (isCartEmpty) {
+//       alan.playText("Your cart is empty")
+//     } else {
+//       setShowCartItems(true)
+//       alan.playText("Opening Cart")
+//     }
+//   }
+
+//   function closeCart(alan) {
+//     if (isCartEmpty) {
+//       alan.playText("Your cart is empty")
+//     } else {
+//       setShowCartItems(false)
+//       alan.playText("Closing Cart")
+//     }
+//   }
+
+//   function addItem(alan, { name, quantity }) {
+//     const item = storeItems.find(
+//       i => i.name.toLowerCase() === name.toLowerCase()
+//     )
+//     if (item == null) {
+//       alan.playText(`I cannot find the ${name} item`)
+//     } else {
+//       addToCart(item.id, quantity)
+//       alan.playText(`Added ${quantity} of the ${name} item to your cart`)
+//     }
+//   }
+
+//   function removeItem(alan, { name }) {
+//     const entry = cart.find(
+//       e => e.item.name.toLowerCase() === name.toLowerCase()
+//     )
+//     if (entry == null) {
+//       alan.playText(`I cannot find the ${name} item in your cart`)
+//     } else {
+//       removeFromCart(entry.itemId)
+//       alan.playText(`Removed the ${name} item from your cart`)
+//     }
+//   }
+
+//   function purchaseItems() {
+//     if (isCartEmpty) {
+//       alan.current.playText("Your cart is empty")
+//     } else {
+//       alan.current.playText("Checking out")
+//       checkout()
+//     }
+//   }
+// }
+
+import { useEffect } from "react"
+import alanBtn from "@alan-ai/alan-sdk-web"
+import { useCart } from "../context/CartContext"
+import storeItems from "../items.json"
+import { deepStrictEqual } from "assert"
+
+const COMMANDS = {
+  OPEN_CART: "open-cart",
+  CLOSE_CART: "close-cart",
+  ADD_ITEM: "add-item",
+  REMOVE_ITEM: "remove-item",
+  PURCHASE_ITEMS: "purchase-items"
+}
+
+export default function useAlan() {
+  const {
+    cart,
+    isCartEmpty,
+    setShowCartItems,
+    addToCart,
+    removeFromCart,
+    checkout
+  } = useCart()
+
+  function openCart(alan) {
+    if (isCartEmpty) {
+      alan.playText("Your cart is empty")
+    } else {
+      setShowCartItems(true)
+      alan.playText("Opening Cart")
+    }
+  }
+
+  function closeCart(alan) {
+    if (isCartEmpty) {
+      alan.playText("Your cart is empty")
+    } else {
+      setShowCartItems(false)
+      alan.playText("Closing Cart")
+    }
+  }
+
+  function addItem(alan, { name, quantity }) {
+    const item = storeItems.find(
+      i => i.name.toLowerCase() === name.toLowerCase()
+    )
+    if (item == null) {
+      alan.playText(`I cannot find the ${name} item`)
+    } else {
+      addToCart(item.id, quantity)
+      alan.playText(`Added ${quantity} of the ${name} item to your cart`)
+    }
+  }
+
+  function removeItem(alan, { name }) {
+    const entry = cart.find(
+      e => e.item.name.toLowerCase() === name.toLowerCase()
+    )
+    if (entry == null) {
+      alan.playText(`I cannot find the ${name} item in your cart`)
+    } else {
+      removeFromCart(entry.itemId)
+      alan.playText(`Removed the ${name} item from your cart`)
+    }
+  }
+
+  function purchaseItems(alan) {
+    if (isCartEmpty) {
+      alan.playText("Your cart is empty")
+    } else {
+      alan.playText("Checking out")
+      checkout()
+    }
+  }
+
+  // deepStrictEqual
+  //
+  //
+
+  useEffect(() => {
+    const alan = alanBtn({
+      key: process.env.REACT_APP_ALAN_KEY,
+      onCommand: ({ command, payload }) => {
+        switch (command) {
+          case COMMANDS.OPEN_CART:
+            openCart(alan)
+            break
+          case COMMANDS.CLOSE_CART:
+            closeCart(alan)
+            break
+          case COMMANDS.ADD_ITEM:
+            addItem(alan, payload)
+            break
+          case COMMANDS.REMOVE_ITEM:
+            removeItem(alan, payload)
+            break
+          case COMMANDS.PURCHASE_ITEMS:
+            purchaseItems(alan)
+            break
+        }
+      }
+    })
+  }, [])
+}
